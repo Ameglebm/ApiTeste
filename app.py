11e7858f3ba0860138690f8c1e552a05b3d44517
ameglebm <ameglevr@gmail.com>
2024-04-13 23:07:50 -0300

    pronto

---
+++ app.py
@@ -0,0 +1,5 @@
+# API - É um lugar para disponibilizar recursos e/ou funcionalidades
+# 1. Objetivo - Criar uma api que disponibiliza a consulta, criação, edição e exclusao de livros
+# 2. ULR base - localhost
+# 3. Endpoints
+# 4. Quais recursos
